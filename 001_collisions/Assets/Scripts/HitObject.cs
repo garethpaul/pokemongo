1603535dc6c0265f5900200ba6eb76113829824d
Gareth Jones <gareth@garethpaul.com>
2016-07-13 22:45:36 -0700

    initial

---
+++ 001_collisions/Assets/Scripts/HitObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitObject : MonoBehaviour
+{
+	public GameObject item; //reference to the Score
+	
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log("You collided");
+		Destroy (item);
+
+	}
+
+	void OnTriggerEnter() //if ball hits collider
+	{
+		// int currentScore = int.Parse(score.GetComponent().text) + 1; //add 1 to the score
+		// score.GetComponent().text = currentScore.ToString();
+		Debug.Log("You triggered an event");
+	}
+
+}
---
+++ 001_collisions/Assets/Scripts/throw.js
@@ -0,0 +1,32 @@
+﻿
+ #pragma strict
+  
+ var power = 1000.0;
+  
+ private var startPos  : Vector3;
+  
+ function OnMouseDown() {
+ 	 Physics.gravity = new Vector3(0, -20, 0);
+     startPos = Input.mousePosition;
+     startPos.z = transform.position.z - Camera.main.transform.position.z;
+     startPos = Camera.main.ScreenToWorldPoint(startPos);
+ }
+  
+ function OnMouseUp() {
+     var endPos = Input.mousePosition;
+     endPos.z = transform.position.z - Camera.main.transform.position.z;
+     endPos = Camera.main.ScreenToWorldPoint(endPos);
+  
+     var force = endPos - startPos;
+     force.z = force.magnitude;
+     force.Normalize();
+  
+     GetComponent.<Rigidbody>().AddForce(force * power);
+     ReturnBall();
+ }
+  
+ function ReturnBall() {
+     yield WaitForSeconds(4.0);
+     transform.position = Vector3.zero;
+     GetComponent.<Rigidbody>().velocity = Vector3.zero;
+ }
